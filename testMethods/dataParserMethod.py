c95d0cc50690bb3b7d8c08e143989fd1a3d707fa
yashsehgal <yashsehgal.work@gmail.com>
2020-11-14 16:17:43 +0530

    new parsing method is added to the project

--- testMethods/dataParserMethod.py
+++ testMethods/dataParserMethod.py
@@ -1,56 +1,60 @@
-# testing data parsing methods and functions
+code = (
+  '<p id="title">Text</p>'
+)
 
-import uuid
-
-datasetForUniqueID = []
-
-def generateNewUniqueID():
-  return uuid.uuid1()
-
-sampleDataset = {
-  "id": "{}".format(generateNewUniqueID()),
-  
-  "componentName": "HeaderCleanComponent_CleanShadow_01",
-  "componentID": "kjfhij-vfbgf987veru8-vfnvjnf",
-  
-  "componentProperties": {
-    "logo": "assets/pathname/filename.png",
-    "headerOptions": {
-      "options": {
-        "home": "/index.htm",
-        "about_us": "/about.htm",
-        "help": "/help.htm",
-        "contact": "/contact.htm",
-        "download": "https://randomSiteName.someDomainID/someRandomUniqueAddress/downloadLink/demoFile.zip"
-      }
-    }
+codeProperties = {
+  "codeLength": 0,
+  "innerHTML": {
+    "parentTag": '',
+    "childTag": '',
+    "TagData": '',
+    "tagID": '',
+    "tagClass": [],
+    "tagIsValid": ''
   }
 }
 
-metaPropertiesData = {
-  "metaProperties": {
-    "componentID": None,
-    "componentAvailability": None,
-    "componentType": None,
-    "componentName": None,
-    "componentProperties": None,
-  }
-}
-
-class TestMethodsToCheckComponentDataProperties:
+class RunTestCaseToCheckParsingMethods:
   def __init__(self):
-    self.sampleDataObject = []
+    self.checkCodeProperties()
+  
+  def checkCodeProperties(self):
+    self.code = code
     
-  def checkComponentProperties(self):
-    self.sampleDataObject.append(sampleDataset)
-    # print(self.sampleDataObject)
-    
-    for keyObject, valueObject in sampleDataset.items():
-      print(keyObject, ':', valueObject)
+    if self.code != None:
+      self.codeLength = 0
+      self.innerHTML = {}
+      self.parentTag = ''
+      self.childTag = '' 
+      self.TagData = '' 
+      self.tagID = '' 
+      self.tagClass = ''
+      self.tagIsValid = False
+      self.tagID = ''
+      for text in code:
+        if self.code != None:
+          # codeProperties.get('codeLength').update(len(self.code))
+          codeProperties.setdefault('codeLength', len(self.code))
+        if code[0] == '<' and code[-1] == '>':
+          self.parentTag = code[1]
+          self.childTag = None
+          for count in range(len(code)):
+            if code[count] == '>' or code[count] == '</':
+              self.TagData.join(code[count])
+            
+          if "id" in code:
+            pointHandler = code.find("id")
+            for count in range(len(code)):
+              if count == pointHandler and code[count+1] == "=" and code[count+2] != None and code[count] != '>':
+                self.tagID.join(code[count])
+
+              
+          # codeProperties.get('innerHTML').get('parentTag').update(self.parentTag)
+          codeProperties.setdefault('parentTag', self.parentTag)
+          # codeProperties.get('innerHTML').get('childTag').update(self.childTag)
+          codeProperties.setdefault('childTag', self.childTag)
+          # codeProperties.get('innerHTML').get('TagData').update(self.TagData)
+          codeProperties.setdefault('TagData', self.tagID)
       
-    self.sampleDataObject.clear()
-
-
-if __name__ == "__main__":
-  testMethodsToCheckComponentDataProperties = TestMethodsToCheckComponentDataProperties()
-  testMethodsToCheckComponentDataProperties.checkComponentProperties()
+if __name__ == '__main__':
+  runTestCaseToCheckParsingMethods = RunTestCaseToCheckParsingMethods()
